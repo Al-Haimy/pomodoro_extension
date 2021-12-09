c10be0c8f63cc8d273e7ad8cf4a8b0a7785eb9b5
40D4Y5 <mohammed1alhaimi@gmail.com>
2021-12-09 19:57:43 +0800

    setting up webpack and manifest json
    setting up the environment so it can work with react as well as working with chrome extension api

---
+++ src/popup.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { render } from "react-dom";
+
+function Popup() {
+  return (
+    <div>
+      <h1>Hello world</h1>
+    </div>
+  );
+}
+render(<Popup />, document.getElementById("popup"));
