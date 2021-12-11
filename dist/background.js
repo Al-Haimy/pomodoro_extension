e5cb1668498587ed9093185cb1f62d930ca34e62
40D4Y5 <mohammed1alhaimi@gmail.com>
2021-12-11 19:41:20 +0800

    editing functions
    fixing functions and organizing the functions

--- dist/background.js
+++ dist/background.js
@@ -1,8 +1,3 @@
-let type = "POMODORO";
-let countDown = 25 * 60;
-let isRunning = false;
-let interval = null;
-let counter = 0;
 const POMODORO = "POMODORO";
 const LONG_BREAK = "LONG BREAK";
 const SHORT_BREAK = "SHORT BREAK";
@@ -11,16 +6,28 @@ const LONG_TIME = 15 * 60;
 const SHORT_TIME = 5 * 60;
 const RUNNING = true;
 const STOPPED = false;
+let type = "POMODORO";
+let countDown = 25 * 60;
+let isRunning = false;
+let interval = null;
+let counter = 0;
 let lifeline;
-
-const audio = new Audio("./alarm.mp3");
-audio.play();
+let typeDigit = 1;
 
 const tick = () => {
   if (countDown > 0) {
     countDown--;
   }
   if (countDown == 0) {
+    chrome.notifications.create("pomodoro", {
+      type: "basic",
+      iconUrl: "logo-128.png",
+      title: "POMODORO TIMER",
+      message: type + " IS DONE!",
+      priority: 0,
+    });
+    const audio = new Audio("./alarm.mp3");
+    audio.play();
     stopPomodoro();
     isRunning = STOPPED;
   }
@@ -38,14 +45,16 @@ chrome.alarms.onAlarm.addListener((alarm) => {
       type: "basic",
       iconUrl: "logo-128.png",
       title: "POMODORO TIMER",
-      message: "You are awesome!",
-      priority: 1,
+      message: type + " IS DONE!",
+      priority: 0,
     });
+    const audio = new Audio("./alarm.mp3");
+    audio.play();
   }
 });
 
 const startPomodoro = () => {
-  interval = setInterval(tick, 1000);
+  interval = setInterval(tick, 10);
   if (countDown >= 60) {
     setAlarmConfig();
   }
@@ -63,28 +72,7 @@ chrome.runtime.onConnect.addListener(function (port) {
   port.onMessage.addListener(function (msg) {
     if (typeof msg.section != "undefined") {
       isRunning = STOPPED;
-      switch (msg.section) {
-        case 1:
-          stopPomodoro();
-          type = POMODORO;
-          countDown = POMO_TIME;
-          break;
-        case 2:
-          stopPomodoro();
-          type = SHORT_BREAK;
-          countDown = SHORT_TIME;
-          break;
-        case 3:
-          stopPomodoro();
-          type = LONG_BREAK;
-          countDown = LONG_TIME;
-          break;
-        default:
-          type = POMODORO;
-          countDown = POMO_TIME;
-
-          break;
-      }
+      typeDigit = msg.section;
     }
     if (typeof msg.action != "undefined") {
       switch (msg.action) {
@@ -98,6 +86,11 @@ chrome.runtime.onConnect.addListener(function (port) {
           break;
       }
     }
+    if (timeShow() != "00") {
+      badge();
+    } else {
+      quiet();
+    }
 
     port.postMessage({
       time: countDown,
@@ -106,3 +99,45 @@ chrome.runtime.onConnect.addListener(function (port) {
     });
   });
 });
+
+const badge = () => {
+  chrome.browserAction.setBadgeBackgroundColor({ color: "#F00" }, () => {
+    chrome.browserAction.setBadgeText({ text: timeShow() });
+  });
+};
+
+const quiet = () => {
+  chrome.browserAction.setBadgeText({});
+};
+
+const timeShow = () => {
+  let minutes = parseInt(countDown / 60) % 60;
+  let seconds = countDown % 60;
+  let timeLift = minutes < 10 ? "0" + minutes : "" + minutes;
+  return timeLift;
+};
+
+const setTypeDuration = (typeName) => {
+  switch (typeName) {
+    case 1:
+      stopPomodoro();
+      type = POMODORO;
+      countDown = POMO_TIME;
+      break;
+    case 2:
+      stopPomodoro();
+      type = SHORT_BREAK;
+      countDown = SHORT_TIME;
+      break;
+    case 3:
+      stopPomodoro();
+      type = LONG_BREAK;
+      countDown = LONG_TIME;
+      break;
+    default:
+      type = POMODORO;
+      countDown = POMO_TIME;
+
+      break;
+  }
+};
