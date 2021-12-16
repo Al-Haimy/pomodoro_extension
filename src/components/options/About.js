e7bc9f0bf26304820588a74e2b518a3e01ec0c43
40D4Y5 <mohammed1alhaimi@gmail.com>
2021-12-16 23:22:31 +0800

    finishing last steps

--- src/components/options/About.js
+++ src/components/options/About.js
@@ -9,29 +9,30 @@ import LinkedInIcon from "@mui/icons-material/LinkedIn";
 import GitHubIcon from "@mui/icons-material/GitHub";
 import Link from "@mui/material/Link";
 import Stack from "@mui/material/Stack";
+import EmailIcon from "@mui/icons-material/Email";
 
 const HEADING_ONE = "What is Pomodor techniuqe ?";
-const HEADING_TWO = "What the extension do ?";
+const HEADING_TWO = "What the extension does ?";
 const HEADING_THREE = "Support";
 const BODY_ONE =
-  "The Pomodoro Technique is a time management\
-   system that encourages people to work with more\
-    efficiny when it comes to time management. It was\
-     developed by a University Student AKA Cerillo.\
-      Cerillo then decided to study with a timer then\
-       he found Tomato (pomodoro in Italian) shaped\
-        kitchen timer, and the pomodoro technique was born.";
+  "The Pomodoro Technique is a time management system that\
+   encourages people to work with more efficiny when it comes\
+    to time management.It was created by AKA Cerillo, a university\
+     student. Cerillo then began to study with a timer, and the\
+      pomodoro technique was formed when he discovered a tomato-shaped\
+       kitchen timer (pomodoro in Italian).";
 const BODY_TWO =
-  "The Extension will work according to the Pomodoro Technique.\
-   All what you need to do is just set the timer and hit start from\
-    the extesnion popup. Note: this extension doesn't collect any data\
-     and it is completely free.";
+  "The Pomodoro Technique will be used to run the Extension.\
+All you have to do is set the timer and press the start button\
+ from the extension window. Note that this addon is absolutely \
+ free and does not gather any data.";
 
 const BODY_THREE =
-  "If you found any bugs or if you have ideas that could\
- help improve this extension please dont hesitate to contact author!.\
-  contact information is listed down below. Also if you liked the extension\
-   don't forget to provide good rate and review. Wish to you all the best :)";
+  "Please do not hesitate to contact the author if you find any\
+   issues or have suggestions on how to improve this extension!\
+    The following is a list of contact information. Also, if you\
+     like the extension, please leave a positive rating and review\
+     . I wish you the best of luck:)";
 
 const About = () => {
   return (
@@ -56,14 +57,22 @@ const About = () => {
           ABOUT
         </Typography>
       </Grid>
-      <Grid item xs={8}>
+      <Grid
+        item
+        xs={12}
+        sx={{
+          paddingLeft: 2,
+          paddingRight: 2,
+          margin: 3,
+        }}
+      >
         <Container
           sx={{
             backgroundColor: "#FBF3E4",
             borderRadius: 0.5,
             paddingBottom: 2,
-            paddingLeft: 2,
-            paddingRight: 2,
+            paddingLeft: 3,
+            paddingRight: 3,
           }}
           maxWidth="xl"
         >
@@ -86,12 +95,32 @@ const About = () => {
               <Divider variant="middle" />
             </Grid>
             <Grid>
+              <TextArea heading="Contact" content=" " />
               <Stack direction="row" spacing={4} alignItems="center">
                 <Link sx={{ color: pink[500] }} href="#" underline="none">
-                  <LinkedInIcon fontSize="large" />
+                  <LinkedInIcon
+                    sx={{
+                      fontSize: 50,
+                    }}
+                  />
                 </Link>
                 <Link sx={{ color: pink[500] }} href="#" underline="none">
-                  <GitHubIcon fontSize="large" />
+                  <GitHubIcon
+                    sx={{
+                      fontSize: 50,
+                    }}
+                  />
+                </Link>
+                <Link
+                  sx={{ color: pink[500] }}
+                  href="mailto:mohammed1alhaimi@gmail.com"
+                  underline="none"
+                >
+                  <EmailIcon
+                    sx={{
+                      fontSize: 50,
+                    }}
+                  />
                 </Link>
               </Stack>
             </Grid>
